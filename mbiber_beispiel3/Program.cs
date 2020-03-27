186fe9af36299a9e8aa678c3c139508af2445fce
Matteo Biber <mbiber@schueler.hakzell.at>
2020-03-27 09:24:12 +0100

    beispiel3 fertig

---
+++ mbiber_beispiel3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mbiber_beispiel3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Console.Write("Geben Sie eine Zahl ein:");
+                int zahl = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Geben Sie eine Zahl ein:");
+                int summand = Convert.ToInt32(Console.ReadLine());
+
+                if (summand == 10)
+                {
+                    throw new Exception("Leider ist eine Addition mit 10 nicht möglich.");
+                }
+
+                int summe = zahl + summand;
+
+                Console.WriteLine("Ergebnis: {0}", summe);
+            }
+            catch
+            {
+                Console.WriteLine("Leider ist eine Addition mit 10 nicht möglich");
+            }
+            
+        }
+    }
+}
