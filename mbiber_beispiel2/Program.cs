dd9f9bdc44ecfd71069216146eaa472938220c9a
Matteo Biber <mbiber@schueler.hakzell.at>
2020-03-27 09:08:09 +0100

    beispiel2 fertig

---
+++ mbiber_beispiel2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mbiber_beispiel2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                    Console.Write("Geben Sie eine Zahl ein durch die dividiert werden soll: ");
+                    int zahl = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Geben Sie eine zweite Nummer ein: ");
+                    int zahl2 = Convert.ToInt32(Console.ReadLine());
+
+                    int erg = zahl / zahl2;
+            
+                    Console.WriteLine("Ergebnis: {0}", erg);
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("Eine Rechnung durch 0 ist leider nicht möglich");
+            }
+            
+
+            
+        }
+
+    }
+}
