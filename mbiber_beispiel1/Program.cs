f3d1091402c4a51e57be8d03cd1fc32a6887022e
Matteo Biber <mbiber@schueler.hakzell.at>
2020-03-27 09:04:17 +0100

    beispiel1 fertig

---
+++ mbiber_beispiel1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mbiber_beispiel1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namen = new string[3];
+
+            try
+            {
+                for (int i = 0; i <= 3 ; i++)
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    namen[i] = name;
+                }
+            }
+            catch(IndexOutOfRangeException)
+            {
+                Console.WriteLine("Sie haben leider zu viele Namen eingegeben");
+            }
+            
+        }
+    }
+}
